0c6c26aa613febe0476ce257b79506428263cbde
Renato Crivano <renato.crivano@gmail.com>
2019-11-07 16:38:44 -0200

    Calling with x-www-form-urlencoded instead of json

--- src/main/java/com/crivano/swaggerservlet/DefaultHTTP.java
+++ src/main/java/com/crivano/swaggerservlet/DefaultHTTP.java
@@ -2,9 +2,15 @@ package com.crivano.swaggerservlet;
 
 import java.io.DataOutputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DefaultHTTP implements IHTTP {
 	public static String convertStreamToString(java.io.InputStream is) {
@@ -28,15 +34,38 @@ public class DefaultHTTP implements IHTTP {
 		String body = null;
 
 		if (req != null && ("POST".equals(method) || "PUT".equals(method))) {
-			body = SwaggerUtils.toJson(req);
+			if (false) {
+				body = SwaggerUtils.toJson(req);
+				con.setRequestProperty("Content-Type", "application/json");
+			} else {
+				StringBuilder sb = new StringBuilder();
+				boolean first = true;
+				for (Field f : req.getClass().getDeclaredFields()) {
+					f.setAccessible(true);
+					Object v = f.get(req);
+					if (v == null)
+						continue;
+					if (v instanceof Date)
+						v = SwaggerUtils.format((Date) v);
+					sb.append(first ? "" : "&");
+					first = false;
+					sb.append(URLEncoder.encode(f.getName()));
+					sb.append("=");
+					sb.append(URLEncoder.encode(v.toString(), "UTF-8"));
+				}
+				body = sb.toString();
+				con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			}
+			byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+			con.setRequestProperty("Content-Length", Integer.toString(bytes.length));
+			con.setRequestProperty("charset", "utf-8");
 
-			con.setRequestProperty("Content-Type", "application/json");
 			con.setConnectTimeout(5000); // set timeout to 5 seconds
 			con.setReadTimeout(20000); // set read timeout to 20 seconds
 			// Send post request
 			con.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			wr.write(body.getBytes(StandardCharsets.UTF_8));
+			wr.write(bytes);
 			wr.flush();
 			wr.close();
 		}
@@ -50,8 +79,7 @@ public class DefaultHTTP implements IHTTP {
 			errorStream = con.getErrorStream();
 			if (errorStream != null)
 				string = convertStreamToString(errorStream);
-			err = (SwaggerError) SwaggerUtils.fromJson(string,
-					SwaggerError.class);
+			err = (SwaggerError) SwaggerUtils.fromJson(string, SwaggerError.class);
 			String errormsg = "HTTP ERROR: " + Integer.toString(responseCode);
 			if (con.getResponseMessage() != null)
 				errormsg = errormsg + " - " + con.getResponseMessage();
@@ -77,5 +105,4 @@ public class DefaultHTTP implements IHTTP {
 		T resp = (T) SwaggerUtils.fromJson(respString, clazzResp);
 		return resp;
 	}
-
 }
