08bf2c98cdb83f5956e6ef64cb1a99185db94933
crivano <renato.crivano@gmail.com>
2019-11-18 16:12:50 -0300

    Error should be serializable

--- src/main/java/com/crivano/swaggerservlet/SwaggerError.java
+++ src/main/java/com/crivano/swaggerservlet/SwaggerError.java
@@ -1,8 +1,11 @@
 package com.crivano.swaggerservlet;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class SwaggerError implements ISwaggerResponse {
+public class SwaggerError implements ISwaggerResponse, Serializable {
+	private static final long serialVersionUID = -6440680646287033407L;
+
 	public static class Detail {
 		String context;
 		String service;
